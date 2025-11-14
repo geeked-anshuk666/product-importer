e4a21fc8070ab218c17f54341dc849b8e3d432dc
Anshuk Jirli <anshuk.jirli2002@gmail.com>
2025-11-14 22:40:03 +0530

    Create views for product and webhook list pages

--- product_importer/views.py
+++ product_importer/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+
+def webhook_list(request):
+    return render(request, 'webhook_list.html')
