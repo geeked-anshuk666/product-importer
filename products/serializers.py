3c27e26b4b262d1a57352f550cbbc0669e08abf9
Anshuk Jirli <anshuk.jirli2002@gmail.com>
2025-11-14 22:25:11 +0530

    Create Product serializer for API endpoints

---
+++ products/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'sku', 'name', 'description', 'is_active', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
