2a3d39c9773134a68636da9ca90d044f3063a1f5
Anshuk Jirli <anshuk.jirli2002@gmail.com>
2025-11-14 22:33:16 +0530

    Configure Celery settings for Redis broker

--- product_importer/settings.py
+++ product_importer/settings.py
@@ -127,6 +127,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Celery Configuration
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
